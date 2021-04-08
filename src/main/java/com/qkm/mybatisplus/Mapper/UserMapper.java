92905a108c0ef07962d9b0b9d52ed27108d46420
QKM-0203 <com_qkm@xiyoulinux.org>
2021-04-08 19:32:49 +0800

    Mybatis-Plus

---
+++ src/main/java/com/qkm/mybatisplus/Mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.qkm.mybatisplus.Mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.qkm.mybatisplus.POJO.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserMapper extends BaseMapper<User> {
+
+}
